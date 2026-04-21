da59ec0abbaf10f0640b1b0a19a2b1731dfe2635
karolsuarez <karolandreasuarezperez0519@gmail.com>
2026-04-20 20:52:50 -0500

    Se realiza validacion de entrada

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import random
 def adivina_numero():
     print("¡Bienvenido a Adivina el Número!")
     numero_secreto = random.randint(1, 10)
-    
-    intento = int(input("Adivina un número entre 1 y 10: "))
-    
+
+    while True:
+        entrada = input("Adivina un número entre 1 y 10: ")
+
+        try:
+            intento = int(entrada)
+            break
+        except ValueError:
+            print("Error: Debes ingresar solo números.")
+
     if intento == numero_secreto:
         print("¡Felicidades, adivinaste el número!")
     else:
-        print(f"Fallaste. El número secreto era {numero_secreto}.")
+        print(f"Fallaste. El número secreto era {numero_secreto}")
 
 if __name__ == "__main__":
     adivina_numero()
